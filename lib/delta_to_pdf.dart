87a072c08b10e7e80b180804b660a329a497d88d
T Muhammed Shijas <t.mohammedshijas@gmail.com>
2024-05-21 17:58:30 +0530

    modify delta to pdf

--- lib/delta_to_pdf.dart
+++ lib/delta_to_pdf.dart
@@ -1,141 +1,117 @@
-/// delta_to_pdf
-/// [title] DeltaToPDF Class
-/// [Description] This is class with helper functions to convert flutter_quill delta object to pdf object
-/// [Written_by] sabeeralikp
+import 'package:flutter_quill/quill_delta.dart';
+import 'package:pdf/widgets.dart';
 
-library delta_to_pdf;
-
-import 'package:flutter_quill/quill_delta.dart' as fq;
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
-
-/// [title] DeltaToPDF Class
-/// [Description] This is class with helper functions to convert flutter_quill delta object to pdf object
-/// [Written_by] sabeeralikp
 class DeltaToPDF {
-  /// [Description]  converts header text style
-  /// [Returns] fontWeight & fontSize
-  getPDFHeaderStyle(key, value) {
-    pw.FontWeight fontWeight = pw.FontWeight.normal;
-    double fontSize = 14;
-    if (value == 1) {
-      fontWeight = pw.FontWeight.bold;
-      fontSize = 24;
-    } else if (value == 2) {
-      fontWeight = pw.FontWeight.bold;
-      fontSize = 20;
-    } else {
-      fontWeight = pw.FontWeight.bold;
-      fontSize = 16;
-    }
-    return [fontWeight, fontSize];
-  }
+  List<Widget> toPDFWidgets(Delta delta) {
+    List<Widget> colWidgets = [];
+    List<InlineSpan> inlineSpanList = [];
 
-  /// [Description]  converts header attributed text style
-  /// [Returns] fontWeight & fontSize
-  getHeaderAttributedText(
-      Map<String, dynamic>? attribute, String text, bool hasAttribute) {
-    pw.FontWeight fontWeight = pw.FontWeight.normal;
-    double fontSize = 14;
-    if (hasAttribute) {
-      attribute!.forEach((key, value) {
-        switch (key) {
-          case "header":
-            if (value == 1) {
-              fontWeight = pw.FontWeight.bold;
-              fontSize = 18;
-            } else if (value == 2) {
-              fontWeight = pw.FontWeight.bold;
-              fontSize = 16;
-            } else {
-              fontWeight = pw.FontWeight.bold;
-              fontSize = 12;
+    for (Operation deltaOp in delta.toList()) {
+      if (deltaOp.isInsert) {
+        String textData = deltaOp.data.toString();
+
+        // Check if textData is multiString
+        print('==============');
+
+        print('Data: ${deltaOp.data}');
+        List<String> lines = textData.split("\n");
+        print("number of lines: ${lines.length}");
+
+        if (textData != "\n") {
+          for (int idx = 0; idx < lines.length; idx++) {
+            String line = lines[idx];
+            textData = line;
+
+            print("Line $idx: $textData");
+
+            // The last line will be processed outside.
+            if (idx < (lines.length - 1)) {
+              // If it's empty, it's just a newline
+              if (textData.isEmpty) {
+                colWidgets.add(SizedBox(height: 12));
+              } else {
+                colWidgets.add(RichText(text: TextSpan(text: textData)));
+              }
             }
-            break;
+          }
         }
-      });
-    }
-    return [fontWeight, fontSize];
-  }
 
-  /// [Description]  converts header attributed text
-  /// [Returns] all text with style
-  getAttributedText(Map<String, dynamic>? attribute, String text,
-      bool hasAttribute, pw.FontWeight fontWeight, double fontSize) {
-    PdfColor fontColor = PdfColor.fromHex("#000");
-    pw.FontStyle fontStyle = pw.FontStyle.normal;
-    pw.TextDecoration decoration = pw.TextDecoration.none;
-    pw.BoxDecoration boxDecoration = const pw.BoxDecoration();
-    if (hasAttribute) {
-      attribute!.forEach((key, value) {
-        switch (key) {
-          case "color":
-            fontColor = PdfColor.fromHex(value);
-            break;
-          case "bold":
-            fontWeight = pw.FontWeight.bold;
-            break;
-          case "italic":
-            fontStyle = pw.FontStyle.italic;
-            break;
-          case "underline":
-            decoration = pw.TextDecoration.underline;
-            break;
-          case "strike":
-            decoration = pw.TextDecoration.lineThrough;
-            break;
-          case "background":
-            boxDecoration = pw.BoxDecoration(color: PdfColor.fromHex(value));
+        FontWeight fontWeight = FontWeight.normal;
+        FontStyle fontStyle = FontStyle.normal;
+        TextDecoration decoration = TextDecoration.none;
+        if (deltaOp.attributes != null) {
+          for (String attribKey in deltaOp.attributes!.keys) {
+            switch (attribKey) {
+              case 'bold':
+                fontWeight = deltaOp.attributes![attribKey]
+                    ? FontWeight.bold
+                    : FontWeight.normal;
+                break;
+              case 'underline':
+                decoration = deltaOp.attributes![attribKey]
+                    ? TextDecoration.underline
+                    : TextDecoration.none;
+                break;
+              case 'italic':
+                fontStyle = deltaOp.attributes![attribKey]
+                    ? FontStyle.italic
+                    : FontStyle.normal;
+                break;
+            }
+          }
         }
-      });
-    }
-    return pw.Text(text,
-        style: pw.TextStyle(
-            color: fontColor,
-            fontWeight: fontWeight,
-            fontStyle: fontStyle,
-            fontSize: fontSize,
-            decoration: decoration,
-            background: boxDecoration,
-            fontFallback: [pw.Font.symbol()]));
-  }
 
-  /// [Description]  main wrapper to convert delta to pdf
-  /// [Returns] pdf widget object
-  deltaToPDF(List<fq.Operation> deltaList) {
-    List header = [null, null];
-    List texts = [];
-    List pdfColumnWidget = [];
-    for (var element in deltaList.reversed) {
-      if (element.data == '\n') {
-        if (header != [] && texts != []) {
-          pdfColumnWidget = pdfColumnWidget +
-              [
-                pw.Row(
-                    children: [...texts.reversed],
-                    crossAxisAlignment: pw.CrossAxisAlignment.start,
-                    mainAxisAlignment: pw.MainAxisAlignment.start)
-              ];
+        inlineSpanList.add(TextSpan(
+            text: textData,
+            style: TextStyle(
+                fontWeight: fontWeight,
+                fontStyle: fontStyle,
+                decoration: decoration)));
+
+        if (textData == "\n" || (deltaOp == delta.toList().last)) {
+          TextAlign textAlign = TextAlign.left;
+          Alignment alignment = Alignment.centerLeft;
+          if (deltaOp.hasAttribute('align')) {
+            switch (deltaOp.attributes!['align']) {
+              case 'center':
+                alignment = Alignment.center;
+                textAlign = TextAlign.center;
+                break;
+              case 'right':
+                alignment = Alignment.centerRight;
+                textAlign = TextAlign.right;
+                break;
+              case 'justify':
+                textAlign = TextAlign.justify;
+                break;
+            }
+          }
+
+          if (deltaOp.hasAttribute('list')) {
+            inlineSpanList.insert(0, const TextSpan(text: "\t\t\t\u2022 "));
+          }
+
+          colWidgets.add(Align(
+              alignment: alignment,
+              child: RichText(
+                  text: TextSpan(
+                    children: inlineSpanList,
+                  ),
+                  textAlign: textAlign)));
+          // Reset the list
+          inlineSpanList = [];
         }
-        texts = [];
-        header = getHeaderAttributedText(element.attributes,
-            element.data.toString(), element.attributes != null);
       } else {
-        texts.add(getAttributedText(
-            element.attributes,
-            element.data.toString(),
-            element.attributes != null,
-            header[0] ?? pw.FontWeight.normal,
-            header[1] ?? 14));
+        print("Unsuppored operation: ${deltaOp.key}");
       }
     }
-    pdfColumnWidget = pdfColumnWidget +
-        [
-          pw.Row(children: [...texts.reversed])
-        ];
-    return pw.Column(
-        children: [...pdfColumnWidget.reversed],
-        crossAxisAlignment: pw.CrossAxisAlignment.start,
-        mainAxisAlignment: pw.MainAxisAlignment.start);
+    print('Column Items: ${colWidgets.length}');
+
+    return colWidgets;
+  }
+
+  Widget toPDFWidget(Delta delta) {
+    final List<Widget> widgets = toPDFWidgets(delta);
+    return Column(children: widgets);
   }
 }
